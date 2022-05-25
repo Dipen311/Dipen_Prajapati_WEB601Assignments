795ce6a6b1c99cfad6f958ac26808cd6e67bfda8
Dipen prajapti <DP183@myscc.ca>
2022-05-24 20:49:04 -0400

    even bg work

--- D_prajapati_Travel/src/app/content-list/content-list.component.ts
+++ D_prajapati_Travel/src/app/content-list/content-list.component.ts
@@ -11,34 +11,34 @@ export class ContentListComponent implements OnInit {
   travelList: Content[];
   constructor() {
     this.travelList = [{
-      id: 0,
+      id: 1,
       title: 'Maldives',
       author: "Dipen",
       imageLink: "https://www.planetware.com/wpimages/2021/07/maldives-best-all-inclusive-resorts-hurawalhi-island-resort-villa.jpg",
       type: "Beach area"
     },{
-      id: 1,
+      id: 2,
       title: 'Dubai',
       author: "Dipen",
       imageLink: "https://cdn.britannica.com/15/189715-050-4310222B/Dubai-United-Arab-Emirates-Burj-Khalifa-top.jpg",
       type: "Advanture"
     },
     {
-      id: 2,
+      id: 3,
       title: 'Abu dhabi',
       author: "Dipen",
       imageLink: "https://www.azamara.com/sites/default/files/heros/abu-dhabi-united-arab-emirates_0.jpg",
       type: "Amazing country"
     },
     {
-      id: 3,
+      id: 4,
       title: 'Singapor',
       author: "Dipen",
       imageLink: "https://cdn.cnn.com/cnnnext/dam/assets/191212182124-04-singapore-buildings.jpg",
       type: "Adventure"
     },
     {
-      id: 4,
+      id: 5,
       title:'Las Vegas',
       author: "Dipen",
       imageLink: 'http://res.cloudinary.com/simpleview/image/upload/v1497480003/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg',
