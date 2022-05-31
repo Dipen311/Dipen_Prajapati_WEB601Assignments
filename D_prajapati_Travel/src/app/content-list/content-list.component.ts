adbbc5732b778123e6614db135604e490a7610b2
Dipen prajapti <DP183@myscc.ca>
2022-05-31 15:18:14 -0400

    array updated

--- D_prajapati_Travel/src/app/content-list/content-list.component.ts
+++ D_prajapati_Travel/src/app/content-list/content-list.component.ts
@@ -28,7 +28,7 @@ export class ContentListComponent implements OnInit {
       title: 'Abu dhabi',
       author: "Dipen",
       imageLink: "https://www.azamara.com/sites/default/files/heros/abu-dhabi-united-arab-emirates_0.jpg",
-      type: "Amazing country"
+      type: "Beach area"
     },
     {
       id: 4,
@@ -42,6 +42,27 @@ export class ContentListComponent implements OnInit {
       title:'Las Vegas',
       author: "Dipen",
       imageLink: 'http://res.cloudinary.com/simpleview/image/upload/v1497480003/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg',
+      type: "Adventure"
+    },
+    {
+      id: 6,
+      title:'Rome',
+      author: "italy",
+      imageLink: 'https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg',
+      type: "History"
+    },
+    {
+      id: 7,
+      title:'Santorini',
+      author: "Greek",
+      imageLink: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/81/30/3f/caption.jpg?w=700&h=-1&s=1&cx=1846&cy=1833&chk=v1_6ae0a81ae3361e988707',
+      type: "Adventure"
+    },
+    {
+      id: 8,
+      title:'Valley of Kings',
+      author: "Egypt",
+      imageLink: 'https://images.mapsofworld.com/allwonders/2013/07/Valley-of-the-Kings.jpg',
       type: "Adventure"
     },
   ];
