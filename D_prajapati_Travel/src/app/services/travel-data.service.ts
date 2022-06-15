9ea0be5ec986dcfab86d10e6ccdde6edfd8c4740
Dipen prajapti <DP183@myscc.ca>
2022-06-15 19:18:02 -0400

    method updated in service

--- D_prajapati_Travel/src/app/services/travel-data.service.ts
+++ D_prajapati_Travel/src/app/services/travel-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { travelList } from '../data/mock-travel-db';
+import { Content } from '../models/content';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,7 +8,26 @@ export class TravelDataService {
 
   constructor() { 
   }
- 
+  getContent(): Content[]{
+    return travelList;
+  }
+  getSingleItem(id: number): Content{
+    return travelList[id];
+  }
+  addContent(newContent: Content): Content[]{
+    travelList.push(newContent);
+    return travelList;
+  }
+  updateContent(id : number, newContent: Content): Content[]{
+    travelList[id] = newContent;
+    return travelList;
+  }
+  removeContent(id:number): Content{
+    let removedContent: Content;
+    removedContent = travelList[id];
+    travelList.splice(id,1);
+    return removedContent;
+  }
 }
 
 
