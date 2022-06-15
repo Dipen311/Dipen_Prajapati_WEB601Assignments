c55ff357a3f5067fe91ff7e4a42ead4542c0e5ca
Dipen prajapti <DP183@myscc.ca>
2022-06-15 19:29:47 -0400

    data fetch in listfile

--- D_prajapati_Travel/src/app/content-list/content-list.component.ts
+++ D_prajapati_Travel/src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Content } from '../models/content';
+import { TravelDataService } from '../services/travel-data.service';
 
 @Component({
   selector: 'app-content-list',
@@ -13,73 +14,18 @@ export class ContentListComponent implements OnInit {
 
   title = "Travel";
   travelList: Content[];
-  constructor() {
-    this.travelList = [{
-      id: 1,
-      title: 'Maldives',
-      author: "Maldives",
-      imageLink: "https://www.planetware.com/wpimages/2021/07/maldives-best-all-inclusive-resorts-hurawalhi-island-resort-villa.jpg",
-      type: "Honeymoon",
-      hashtags : [ 'Maldives', 'Beach area']
-    },{
-      id: 2,
-      title: 'Dubai',
-      author: "Dubai",
-      imageLink: "https://cdn.britannica.com/15/189715-050-4310222B/Dubai-United-Arab-Emirates-Burj-Khalifa-top.jpg",
-      type: "adventure",
-      hashtags : [ 'Dubai', 'Real estate']
-    },
-    {
-      id: 3,
-      title: 'Abu dhabi',
-      author: "Dubai",
-      imageLink: "https://www.azamara.com/sites/default/files/heros/abu-dhabi-united-arab-emirates_0.jpg",
-      type: "adventure",
-      hashtags : [ 'Abu dhabi', 'Amazing city']
-    },
-    {
-      id: 4,
-      title: 'Singapor',
-      author: "Singapor",
-      imageLink: "https://cdn.cnn.com/cnnnext/dam/assets/191212182124-04-singapore-buildings.jpg",
-      type: "Honeymoon",
-      hashtags : [ 'Singapor', 'Amazing']
-    },
-    {
-      id: 5,
-      title:'Las Vegas',
-      author: "Clark",
-      imageLink: '',
-      type: "adventure",
-      hashtags : [ 'Las Vegas', 'Amazing']
-    },
-    {
-      id: 6,
-      title:'Rome',
-      author: "italy",
-      imageLink: 'https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg',
-      type: "Honeymoon",
-      hashtags : [ 'Rome', 'Historical place']
-    },
-    {
-      id: 7,
-      title:'Santorini',
-      author: "Greek",
-      imageLink: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/81/30/3f/caption.jpg?w=700&h=-1&s=1&cx=1846&cy=1833&chk=v1_6ae0a81ae3361e988707',
-      type: "Honeymoon",
-      hashtags : [ 'Santorini', 'White and blue Theme']
-    },
-    {
-      id: 8,
-      title:'Valley of Kings',
-      author: "Egypt",
-      imageLink: 'https://images.mapsofworld.com/allwonders/2013/07/Valley-of-the-Kings.jpg',
-      hashtags : [ 'Valley of Kings', 'Amazing']
-    },
-  ];
+  singleTravel: Content | undefined;
+  constructor(private travelService : TravelDataService) {
+    this.travelList = [];
+  
   }
   ngOnInit(): void {
+    
+    this.travelList = this.travelService.getContent();
+    this.singleTravel = this.travelService.getSingleItem(5);
+
     throw new Error('Method not implemented.');
+    
   }
 
   updatePage(cardNameOnTheTypescriptSide: string): void {
